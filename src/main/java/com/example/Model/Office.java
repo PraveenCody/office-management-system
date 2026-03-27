6e6f723ac271ef79da14570a5cf4c76cea3516cd
Praveen <iampraveen435@gmail.com>
2026-03-27 20:18:24 +0530

    Upto Model Commited

---
+++ src/main/java/com/example/Model/Office.java
@@ -0,0 +1,39 @@
+package com.example.Model;
+
+public class Office {
+
+    int id;
+    String name;
+    String dept;
+
+    public Office(int id,String name,String dept){
+        this.id = id;
+        this.name = name;
+        this.dept = dept;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+}
